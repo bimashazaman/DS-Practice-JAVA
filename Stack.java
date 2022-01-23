b94d2876ca687786f57bbaaaeda27b74c9fa00de
bimashazaman <bimashazaman1234@gmail.com>
2022-01-24 04:29:09 +0600

    inital commit

---
+++ Stack.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+
+public class Main 
+{
+    public static void main(String[] args)
+{
+    Stack<String> stack = new Stack<String>();
+
+    System.out.println("Stack is empty: " + stack.isEmpty());
+    stack.push("bimasha");
+    stack.push("bimasha4");
+    stack.push("bimasha4");
+    stack.push("bimasha46");
+    stack.push("bimasha468");
+
+    System.out.println("Stack is empty: " + stack.isEmpty());
+    System.out.println("Stack size: " + stack.size());
+    System.out.println("Stack peek: " + stack.peek());
+    System.out.println("Stack pop: " + stack.pop());
+    System.out.println("Stack peek: " + stack.peek());
+
+    System.out.println("stack size: " + stack.size());
+    System.out.println(stack);
+}
+}
---
+++ what_is_ds.java
@@ -0,0 +1,36 @@
+//ds is a named location that can be used to store data. 
+
+//e.g. a stack, queue, hash table, etc. why is this important?  what is the difference between a stack and a queue?  what is the difference between a hash table and a linked list?  what is the difference between a hash table and a binary search tree?   
+
+
+//what is array?  array is a collection of elements stored at the contiguous memory locations. like char[] array; 
+
+
+//what is algorithm?  algorithm is a set of rules that can be applied to a set of data. e.g. sorting, searching, etc.
+
+//what is stack?  stack is a data structure that can be used to store data in a LIFO (last in first out) manner. push() and pop() are used to add and remove data from the stack. Lifo is last in first out. it is like a vertical tower
+
+                // |________4______|  -> TO ADD 4 TO THE STACK USE push(4) 
+                // |________4______|                    -> To remove 4 from the stack use pop()
+                // |________3______|  -> TO ADD 3 TO THE STACK USE push(3)
+                // |_______4_______|   
+                // |_____4_________|
+                // |________8______|
+                // |________8______|
+                // |_______4_______|
+                //
+                //          ||
+                //          ||
+                //          ||  
+                //          \/
+                //peek returns
+                 the top element of the stack.
+                //pop returns the top element of the stack and removes it from the stack.
+                //push adds an element to the top of the stack.
+                //isEmpty returns true if the stack is empty.
+                //size returns the number of elements in the stack.
+
+
+//what is queue?  queue is a data structure that can be used to store data in a FIFO (first in first out) manner. enqueue() and dequeue() are used to add and remove data from the queue. FIFO is first in first out. it is like a horizontal tower 
+
+
